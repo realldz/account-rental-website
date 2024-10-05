4136ce54a07e9bbb4f36c0e1bbbda2811a049d54
TonikakuKawaii <31581520+realldz@users.noreply.github.com>
2024-10-05 15:59:10 +0700

    feat: add some factories

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->unique()->word,
+            'description' => $this->faker->sentence,
+            'price' => $this->faker->randomFloat(2, 1, 100),
+            'category_id' => \App\Models\Category::factory(),
+        ];
+    }
+}
