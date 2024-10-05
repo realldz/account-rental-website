a1ef62860cec0f6d1abd888f4755ca611755ee8f
TonikakuKawaii <31581520+realldz@users.noreply.github.com>
2024-10-06 01:32:25 +0700

    feat: add product circles, comment tables

---
+++ database/migrations/2024_10_05_101757_create_comments_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('product_id');
+            $table->foreign('product_id')->references('id')->on('products');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->unsignedBigInteger('parent_id')->nullable(); // Thêm cột này để cho phép trả lời comment
+            $table->foreign('parent_id')->references('id')->on('comments')->onDelete('cascade');
+            $table->string('content');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+}
---
+++ database/migrations/2024_10_05_181633_create_product_circles_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductCirclesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_circles', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('product_id'); // Khóa ngoại tham chiếu tới sản phẩm
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
+            $table->integer('cycle_price'); // Giá cho chu kỳ
+            $table->string('cycle_unit'); // Đơn vị chu kỳ (ngày, tuần, tháng)
+            $table->integer('cycle_value'); // Giá trị chu kỳ (1, 2, 3,...)
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product_circles');
+    }
+}
