7f11a12d48cd3bd97b07d8c5c5ffe5aaf59566a8
TonikakuKawaii <31581520+realldz@users.noreply.github.com>
2024-10-07 00:09:45 +0700

    feat: admin, user route

---
+++ routes/admin.php
@@ -0,0 +1,8 @@
+<?php
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Support\Facades\Route;
+
+Route::prefix('admin')->name('admin.')->group(function () {
+    Route::get('/', [AdminController::class, 'index'])->name('index');
+});
