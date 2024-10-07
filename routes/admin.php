4903d50f31c33c266763104c7f030831eede394f
TonikakuKawaii <31581520+realldz@users.noreply.github.com>
2024-10-08 00:13:37 +0700

    fix: use resource route

--- routes/admin.php
+++ routes/admin.php
@@ -5,5 +5,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('admin')->name('admin.')->group(function () {
     Route::get('/', [AdminController::class, 'index'])->name('index');
-    Route::get('users', [UserController::class, 'index'])->name('users');
+    // Route::prefix('users')->name('users.')->group(function () {
+    //     Route::get('/', [UserController::class, 'index'])->name('index');
+    //     Route::get('/{user}', [UserController::class, 'index'])->name('edit');
+    // });
+    Route::resource('users', UserController::class)->except(['show']);
 });
