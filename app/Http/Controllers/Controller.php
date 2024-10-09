56fb207789e63b1ba7120434137026ad79653cee
TonikakuKawaii <31581520+realldz@users.noreply.github.com>
2024-10-10 02:37:00 +0700

    chore: remove old json response function

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -5,24 +5,9 @@ namespace App\Http\Controllers;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
-use Illuminate\Http\JsonResponse;
 use Illuminate\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
-    public function success($message, $code = 200): JsonResponse
-    {
-        return response()->json([
-            'status' => 'success',
-            'message' => $message,
-        ], $code);
-    }
-    public function fail($message, $code = 200): JsonResponse
-    {
-        return response()->json([
-            'status' => 'fail',
-            'message' => $message,
-        ], $code);
-    }
 }
