2400c3a781669a64f463f51d3e8a486e15667b11
TonikakuKawaii <31581520+realldz@users.noreply.github.com>
2024-10-15 00:46:51 +0700

    refactor: globally delete function

---
+++ resources/views/admin/components/delete-script.blade.php
@@ -0,0 +1,31 @@
+<script>
+    function deleteObj(url, name) {
+        swal.fire({
+            title: 'Warning',
+            text: 'Xác nhận xoá【' + name + '】 ？',
+            icon: 'warning',
+            showCancelButton: true,
+            cancelButtonText: 'Đóng',
+            confirmButtonText: 'Xác nhận',
+        }).then((result) => {
+            if (result.value) {
+                $.ajax({
+                    method: 'DELETE',
+                    url: url,
+                    data: {_token: '{{ csrf_token() }}'},
+                    dataType: 'json',
+                    success: function(ret) {
+                        if (ret.status === 'success') {
+                            swal.fire({title: ret.message, icon: 'success', timer: 1000, showConfirmButton: false}).then(() => window.location.reload());
+                        } else {
+                            swal.fire({title: ret.message, icon: 'error'}).then(() => window.location.reload());
+                        }
+                    },
+                    error: function(ret) {
+                        swal.fire({title: ret, icon: 'error'}).then(() => window.location.reload());
+                    }
+                });
+            }
+        });
+    }
+</script>
--- resources/views/admin/pages/user/index.blade.php
+++ resources/views/admin/pages/user/index.blade.php
@@ -60,7 +60,7 @@
                                 </button>
                                 <div class="dropdown-menu">
                                     <a class="dropdown-item" href="{{ route('admin.user.edit', $user->id) }}">Chỉnh sửa</a>
-                                    <a class="dropdown-item" href="javascript:deleteUser('{{ route('admin.user.destroy', $user->id) }}', '{{ $user->name }}')">Xoá</a>
+                                    <a class="dropdown-item" href="javascript:deleteObj('{{ route('admin.user.destroy', $user->id) }}', '{{ $user->name }}')">Xoá</a>
                                 </div>
                             </div>
                         </td>
@@ -77,35 +77,5 @@
     </x-adminlte-card>
 @stop
 @push('js')
-    <script>
-        function deleteUser(url, username) {
-            swal.fire({
-                title: 'Warning',
-                text: 'Confirm to delete user 【' + username + '】 ？',
-                icon: 'warning',
-                showCancelButton: true,
-                cancelButtonText: 'Đóng',
-                confirmButtonText: 'Xác nhận',
-            }).then((result) => {
-                if (result.value) {
-                    $.ajax({
-                        method: 'DELETE',
-                        url: url,
-                        data: {_token: '{{ csrf_token() }}'},
-                        dataType: 'json',
-                        success: function(ret) {
-                            if (ret.status === 'success') {
-                                swal.fire({title: ret.message, icon: 'success', timer: 1000, showConfirmButton: false}).then(() => window.location.reload());
-                            } else {
-                                swal.fire({title: ret.message, icon: 'error'}).then(() => window.location.reload());
-                            }
-                        },
-                        error: function(ret) {
-                            swal.fire({title: ret, icon: 'error'}).then(() => window.location.reload());
-                        }
-                    });
-                }
-            });
-        }
-    </script>
+@include('admin.components.delete-script')
 @endpush
