6478901326e2af97d260e9e75b38d2d45e2f535d
TonikakuKawaii <31581520+realldz@users.noreply.github.com>
2024-11-14 01:30:29 +0700

    feat: add payment

---
+++ database/migrations/2024_11_13_105556_create_payment_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePaymentTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tables', function (Blueprint $table) {
+            $table->id();
+            $table->string('code')->unique()->index();
+            $table->string('name');
+            $table->string('description')->nullable();
+            $table->json('config');
+            $table->string('icon')->nullable();
+            $table->boolean('enable')->default(0);
+            $table->integer('sort')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tables');
+    }
+}
