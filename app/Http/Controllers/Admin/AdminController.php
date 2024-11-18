47c3b20be0e50ef48c461d312cc691c8fc209035
TonikakuKawaii <31581520+realldz@users.noreply.github.com>
2024-11-19 01:48:01 +0700

    feat: add some info for admin dashboard

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -16,6 +16,27 @@ class AdminController extends Controller
         $totalProduct = Product::count();
         $totalAccount = Account::count();
         $totalOrder = Order::count();
-        return view('admin.index', compact('totalUser', 'totalProduct', 'totalAccount', 'totalOrder'));
+        $newUserToday = User::where('created_at', '>=', date('Y-m-d 00:00:00'))->count();
+        $newOrderToday = Order::where('created_at', '>=', date('Y-m-d 00:00:00'))->count();
+        $newOrderSuccessToday = Order::where('created_at', '>=', date('Y-m-d 00:00:00'))->where('status', 1)->count();
+        $productForSale = Product::where('status', 1)->count();
+        $revenueToday = Order::where('created_at', '>=', date('Y-m-d 00:00:00'))->where('status', 1)->sum('total_price');
+        $revenueMonth = Order::where('created_at', '>=', date('Y-m-1 00:00:00'))->where('status', 1)->sum('total_price');
+        $revenueTotal = Order::where('status', 1)->sum('total_price');
+        $accountRenting = Account::where('status', 1)->count();
+        return view('admin.index', compact(
+            'totalUser',
+            'totalProduct', 
+            'totalAccount', 
+            'totalOrder', 
+            'newUserToday', 
+            'newOrderToday', 
+            'newOrderSuccessToday', 
+            'productForSale', 
+            'revenueToday',
+            'revenueMonth',
+            'revenueTotal',
+            'accountRenting'
+        ));
     }
 }
--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -24,6 +24,38 @@
             <x-adminlte-small-box title="{{ $totalOrder }}" text="Đơn hàng" icon="fas fa-shopping-cart" theme="success"
                 url="#" url-text="View details" />
         </div>
+        <div class="col-lg-3 col-6">
+            <x-adminlte-small-box title="{{ $newUserToday }}" text="Người dùng mới hôm nay" icon="fas fa-shopping-cart" theme="success"
+                url="#" url-text="View details" />
+        </div>
+        <div class="col-lg-3 col-6">
+            <x-adminlte-small-box title="{{ $newOrderToday }}" text="Đơn hàng mới hôm nay" icon="fas fa-shopping-cart" theme="success"
+                url="#" url-text="View details" />
+        </div>
+        <div class="col-lg-3 col-6">
+            <x-adminlte-small-box title="{{ $newOrderSuccessToday }}" text="Đơn hàng đã hoàn thành hôm nay" icon="fas fa-shopping-cart" theme="success"
+                url="#" url-text="View details" />
+        </div>
+        <div class="col-lg-3 col-6">
+            <x-adminlte-small-box title="{{ $productForSale }}" text="Sản phẩm đang bán" icon="fas fa-shopping-cart" theme="success"
+                url="#" url-text="View details" />
+        </div>
+        <div class="col-lg-3 col-6">
+            <x-adminlte-small-box title="{{ number_format($revenueToday, 0, ',', ',') }}" text="Doanh thu hôm nay" icon="fas fa-shopping-cart" theme="success"
+                url="#" url-text="View details" />
+        </div>
+        <div class="col-lg-3 col-6">
+            <x-adminlte-small-box title="{{ number_format($revenueMonth, 0, ',', ',') }}" text="Doanh thu tháng" icon="fas fa-shopping-cart" theme="success"
+                url="#" url-text="View details" />
+        </div>
+        <div class="col-lg-3 col-6">
+            <x-adminlte-small-box title="{{ number_format($revenueTotal, 0, ',', ',') }}" text="Tổng doanh thu" icon="fas fa-shopping-cart" theme="success"
+                url="#" url-text="View details" />
+        </div>
+        <div class="col-lg-3 col-6">
+            <x-adminlte-small-box title="{{ $accountRenting }}" text="Tài khoản đang được thuê" icon="fas fa-shopping-cart" theme="success"
+                url="#" url-text="View details" />
+        </div>
 
     </div>
 
