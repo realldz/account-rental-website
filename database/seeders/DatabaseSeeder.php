6b86f17b5f8c217586bd2b72a9c60db5aaf6c074
TonikakuKawaii <31581520+realldz@users.noreply.github.com>
2024-11-30 23:34:47 +0700

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,13 +19,13 @@ class DatabaseSeeder extends Seeder
             'password' => bcrypt('123456'),
             'is_admin' => true,
         ]);
-        \App\Models\User::factory(10)->create();
-        \App\Models\Category::factory(5)->create();
-        \App\Models\Product::factory(10)->create();
-        \App\Models\Account::factory(10)->create();
-        \App\Models\ProductCycle::factory(10)->create();
-        \App\Models\Order::factory(10)->create();
-        \App\Models\OrderItem::factory(20)->create();
-        \App\Models\Comment::factory(20)->create();
+        // \App\Models\User::factory(10)->create();
+        // \App\Models\Category::factory(5)->create();
+        // \App\Models\Product::factory(10)->create();
+        // \App\Models\Account::factory(10)->create();
+        // \App\Models\ProductCycle::factory(10)->create();
+        // \App\Models\Order::factory(10)->create();
+        // \App\Models\OrderItem::factory(20)->create();
+        // \App\Models\Comment::factory(20)->create();
     }
 }
